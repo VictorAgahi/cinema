67737e479626dc2eb41f512163ebd799feef5cc6
damien.lugagne-delpon <damien.lugagne-delpon@epita.fr>
2026-04-28 13:15:25 +0200

    fix

--- ton-super-nom-de-projet/src/app/swipe/swipe.ts
+++ ton-super-nom-de-projet/src/app/swipe/swipe.ts
@@ -17,43 +17,32 @@ export enum SwipeDirection {
   styleUrls: ['./swipe.scss']
 })
 export class SwipeComponent implements OnInit {
-  private cartService = inject(CartService);
-  private moviesApi = inject(MoviesApi);
+  private readonly cartService = inject(CartService);
+  private readonly moviesApi  = inject(MoviesApi);
 
   readonly SwipeDirection = SwipeDirection;
-  movies = signal<Movie[]>([]);
+  movies       = signal<Movie[]>([]);
   currentIndex = signal(0);
-  swipeState = signal<SwipeDirection | null>(null);
-
-  ngOnInit() {
-    this.moviesApi.getMovies().subscribe(backendMovies => {
-      this.movies.set(backendMovies);
-    });
-  }
+  swipeState   = signal<SwipeDirection | null>(null);
 
   currentMovie = computed(() => this.movies()[this.currentIndex()]);
 
-  visibleMovies = computed(() => {
-    const idx = this.currentIndex();
-    const all = this.movies();
-    const result: Movie[] = [];
-    if (idx + 1 < all.length) result.push(all[idx + 1]);
-    if (idx < all.length) result.push(all[idx]);
-    return result;
-  });
+  ngOnInit() {
+    this.moviesApi.getMovies().subscribe(m => this.movies.set(m));
+  }
 
   like() {
     if (this.swipeState()) return;
     this.swipeState.set(SwipeDirection.RIGHT);
     const movie = this.currentMovie();
     if (movie) this.cartService.addToCart(movie);
-    setTimeout(() => this.next(), 500);
+    setTimeout(() => this.next(), 450);
   }
 
   dislike() {
     if (this.swipeState()) return;
     this.swipeState.set(SwipeDirection.LEFT);
-    setTimeout(() => this.next(), 500);
+    setTimeout(() => this.next(), 450);
   }
 
   private next() {
